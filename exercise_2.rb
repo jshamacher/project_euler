15e8febc2308b7d99361f348e51485963544fb91
Josh Hamacher <hamacher@brutallogic.net>
2021-02-28 11:19:21 -0800

    Added simple driver program.
    As expected, the performance of the existing fibonacci
    implementation is unusably poor.  I ran out of patience
    waiting for it do something.

---
+++ exercise_2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative 'math'
+
+current_index = 0
+current_term = 0
+sum = 0
+
+while true do
+  current_term = Math.fibonacci(current_index)
+  break if current_term > 4000000
+
+  sum += current_term if (current_term % 2) == 0
+  current_term += 1
+end
+
+puts "Sum of even-valued fibonacci terms with value no greater than 4,000,000: #{sum}"
