eaf0b3bcd5c9b1c20dc1c15a8590866ba1d863ae
Josh Hamacher <hamacher@brutallogic.net>
2021-02-28 11:41:19 -0800

    I had a bug in this, which explains why everything was so slow.

--- exercise_2.rb
+++ exercise_2.rb
@@ -11,7 +11,7 @@ while true do
   break if current_term > 4000000
 
   sum += current_term if (current_term % 2) == 0
-  current_term += 1
+  current_index += 1
 end
 
 puts "Sum of even-valued fibonacci terms with value no greater than 4,000,000: #{sum}"
