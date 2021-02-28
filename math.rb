3f6dde75fcb6efc9dd87eeed66bec813a0c36202
Josh Hamacher <hamacher@brutallogic.net>
2021-02-28 11:24:42 -0800

    Switch from recursive to iterative implementation.  Still unusably slow.

--- math.rb
+++ math.rb
@@ -4,6 +4,19 @@ module Math
 
     return 1 if index == 0
     return 2 if index == 1
-    return fibonacci(index - 2) + fibonacci(index - 1)
+
+    prev_prev_value = 1
+    prev_value = 2
+    current_value = 3
+
+    current_index = 2
+    while current_index <= index
+      current_index += 1
+
+      current_value = prev_prev_value + prev_value
+      prev_prev_value = prev_value
+      prev_value = current_value
+    end
+    return current_value
   end
 end
